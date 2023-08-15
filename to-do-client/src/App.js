1bcf1657d13d00b60bc4e99aaa8768a126eafff3
José Vinicius <93006559+joseviniciusb@users.noreply.github.com>
2023-08-15 20:38:12 +0000

    feat: adicionar estrutura inicial do projeto, em hard code

--- to-do-client/src/App.js
+++ to-do-client/src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     <h1>ToDoListss</h1>
+        <h1>ToDoListss</h1>
       </header>
+
+      <div className="AddTaskInput">
+        <input type="text"></input>
+        <button>Criar tarefa</button>
+      </div>
+
+      <div className="TasksContainer">
+        <span className="TaskCount">{"Tarefas criadas:" + 0}</span>
+        <span className="CompletedTaskCounte">
+          {"Tarefas concluidas:" + 0 + "de" + 0}
+        </span>
+
+        <div className="Task">
+          <input type="checkbox"></input>
+          <span>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s
+          </span>
+          <span className="DeleteTask">X</span>
+        </div>
+      </div>
     </div>
   );
 }
